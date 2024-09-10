e03e3b914b60b21bdc39bbbd6e8a464980fb98ed
FernandoH94 <fernando.hsousa@senacsp.edu.br>
2024-09-10 19:23:04 -0300

    feat: outra forma da questão 5

--- exercicio_05.js
+++ exercicio_05.js
@@ -3,67 +3,69 @@ vetor. Armazene os números pares no vetor PAR e os números IMPARES
 no vetor impar. Deve-se no final mostrar os três vetores.
 */
 
-let listaNumeros = [];
+function lerNumeros() {
+
+    let listaNumeros = [];
+    //Leitura de Numeros e Inserindo no Lista de Números
+    for (let i = 0; i < 10; i++) {
+        let numeroString = prompt("Informe um Número:");
+        let numero = parseInt(numeroString);
+        listaNumeros.push(numero);
+    }
+
+    return listaNumeros;
+
+}
+
+function guardarParesImpares(lista) {
+
+    //Processar os pares e impares
+    for (let j = 0; j < lista.length; j++) {
+        //Pega o valor corrente do vetor de números
+        let numeroAgora = lista[j];
+        //Verifica se ePar == true ou false
+        let ehPar = ((numeroAgora % 2) == 0);
+        if (ehPar) {
+            listaPares.push(numeroAgora);
+        } else {
+            listaImpares.push(numeroAgora);
+        }
+    }
+}
+
+function mostrarVetor(vetor) {
+    let mensagem = "";
+    for (let k = 0; k < vetor.length; k++) {
+        let ultimaPosicao = (vetor.length - 1);
+        if (k == ultimaPosicao) {
+            mensagem += vetor[k] + ".";
+        }
+        else {
+            mensagem += vetor[k] + ", ";
+        }
+    }
+    return mensagem;
+}
+//faço a chamada da função de leitura de numeros: nessa função o vetor fica declarado na função e devolve a lista cheia.
+let listaPrincipal = lerNumeros();
+
 let listaPares = [];
 let listaImpares = [];
 
-//Leitura de Numeros e Inserindo no Lista de Números
-for (let i = 0; i < 10; i++) {
-    let numeroString = prompt("Informe um Número:");
-    let numero = parseInt(numeroString);
-    listaNumeros.push(numero);
-}
+//faço a chamada da função que insere os numeros da listaPrincipal na lista de par ou impar
+guardarParesImpares(listaPrincipal);
 
-//Processar os pares e impares
-for (let j = 0; j < listaNumeros.length; j++) {
-    //Pega o valor corrente do vetor de números
-    let numeroAgora = listaNumeros[j];
-    //Verifica se ePar == true ou false
-    let ehPar = ((numeroAgora % 2) == 0);
+// faço uma chamada a função que pega o conteúdo da listaPrincipal e monto um texto com o conteúdo dela
+let textoListaInteira =  "Principal: " + mostrarVetor(listaPrincipal) + "\n";
 
-    if (ehPar) {
-        listaPares.push(numeroAgora);
-    } else {
-        listaImpares.push(numeroAgora);
-    }
-}
+// faço uma chamada a função que pega o conteúdo da listaPares e monto um texto com o conteúdo dela
+let textoPares =  "Pares: " + mostrarVetor(listaPares) + "\n";
 
-//Mostrar todos os numeros da lista Principal
-let mensagemNumeros = "";
-for (let k = 0; k < listaNumeros.length; k++) {
-    let ultimaPosicao = (listaNumeros.length - 1);
-    if (k == ultimaPosicao) {
-        mensagemNumeros += listaNumeros[k] + ".";
-    }
-    else {
-        mensagemNumeros += listaNumeros[k] + ", ";
-    }
-}
+// faço uma chamada a função que pega o conteúdo da listaImpares e monto um texto com o conteúdo dela
+let textoImpares =  "Impares: " + mostrarVetor(listaImpares);
 
-//Mostrar todos os numeros da lista de Pares
-let mensagemPares = "";
-for (let k = 0; k < listaPares.length; k++) {
-    let ultimaPosicao = (listaPares.length - 1);
-    if (k == ultimaPosicao) {
-        mensagemPares += listaPares[k] + ".";
-    }
-    else {
-        mensagemPares += listaPares[k] + ", ";
-    }
-}
 
-//Mostrar todos os numeros da lista de Impares
-let mensagemImpares = "";
-for (let k = 0; k < listaImpares.length; k++) {
-    let ultimaPosicao = (listaImpares.length - 1);
-    if (k == ultimaPosicao) {
-        mensagemImpares += listaImpares[k] + ".";
-    }
-    else {
-        mensagemImpares += listaImpares[k] + ", ";
-    }
-}
+// junto os conteúdos em uma variavel de texto final para exibir em um alerta só.
+let textoFinal = textoListaInteira + textoPares + textoImpares;
 
-alert(`Os Números inseridos: ${mensagemNumeros} 
-       Os Números pares: ${mensagemPares} 
-       Os Números impares: ${mensagemImpares}`);
+alert(textoFinal);
